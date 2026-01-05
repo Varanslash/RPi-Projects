00545489469fe9d3903d27ef82b2ce2d9e420c12
Varanslash <odiaenvy@gmail.com>
2026-01-04 17:09:29 -0800

    source code

---
+++ kernel8/kernel8.asm
@@ -0,0 +1,10 @@
+.global _start
+_start:
+    mov x0, #3
+    mov x1, #4
+    mul x0, x0, x0 @ x0 = x0 x x0
+    mul x1, x1, x1 @ same here for x1
+    add x2, x1, x0
+
+nomovingforu:
+    b nomovingforu
